15567b615e63806822f139e2c9d8830700c4c335
Fajri Aulia <fajriauzahran@gmail.com>
2026-06-09 02:53:47 +0700

    integrate influxdb sensor_pekerja measurement5

--- public/dashboard.js
+++ public/dashboard.js
@@ -3,22 +3,21 @@ async function fetchData() {
         const res = await fetch("/api/latest");
         const data = await res.json();
 
-        console.log("📊 DATA:", data);
+        console.log("DATA:", data);
 
-        document.getElementById("suhu").innerText = data.suhu;
-        document.getElementById("detak").innerText = data.detak;
-        document.getElementById("ch4").innerText = data.CH4;
-        document.getElementById("co").innerText = data.CO;
-        document.getElementById("h2s").innerText = data.H2S;
+        document.getElementById("suhu").innerText = data.suhu ?? "--";
+        document.getElementById("detak").innerText = data.detak ?? "--";
+        document.getElementById("ch4").innerText = data.CH4 ?? "--";
+        document.getElementById("co").innerText = data.CO ?? "--";
+        document.getElementById("h2s").innerText = data.H2S ?? "--";
 
-        document.getElementById("lat").innerText = data.lat;
-        document.getElementById("lon").innerText = data.lon;
+        document.getElementById("lat").innerText = data.lat ?? "--";
+        document.getElementById("lon").innerText = data.lon ?? "--";
 
     } catch (err) {
         console.error("Dashboard error:", err);
     }
 }
 
-// auto refresh
 setInterval(fetchData, 2000);
 fetchData();
